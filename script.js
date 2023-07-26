84630873030ab84fbbed0f83135583ef15709cc8
mgmarco <mgmarco38@gmail.com>
2023-07-26 17:00:52 -0300

    Navbar finalizado 2/2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function menuShow() {
-  var mobileMenu = document.querySelector('.mobile-menu');
-  var mobileMenuButton = document.querySelector('.mobile-menu-icon button');
-
-  if (mobileMenu.style.display === 'flex') {
-    mobileMenu.style.display = 'none';
-    mobileMenuButton.innerHTML = '&#9776;'; // Ícone de três barras horizontais quando o menu está oculto
-  } else {
-    mobileMenu.style.display = 'flex';
-    mobileMenuButton.innerHTML = 'X'; // Texto "X" para representar o ícone "X"
+    var mobileMenu = document.querySelector('.mobile-menu');
+    var mobileMenuButton = document.querySelector('.mobile-menu-icon button');
+  
+    if (mobileMenu.style.display === 'flex') {
+      mobileMenu.style.display = 'none';
+      mobileMenuButton.innerHTML = '&#9776;'; // Ícone de três barras horizontais quando o menu está oculto
+    } else {
+      mobileMenu.style.display = 'flex';
+      mobileMenuButton.innerHTML = 'X'; // Texto "X" para representar o ícone "X"
+    }
   }
-}
+  
