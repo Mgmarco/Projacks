d0423b52cf6616cda2b0a044c48b320ad5b13914
BCampos <bccampos105@gmail.com>
2023-07-25 22:19:02 -0300

    Alteração no Nav bar Icone "X" aprece quando o menu é mostrado

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 function menuShow() {
-    let mobileMenu = document.querySelector('.mobile-menu');
-    let mobileMenuButton = document.querySelector('.mobile-menu-icon button');
-  
-    if (mobileMenu.style.display === 'flex') {
-      mobileMenu.style.display = 'none';
-      mobileMenuButton.classList.remove('collapsed');
-    } else {
-      mobileMenu.style.display = 'flex';
-      mobileMenuButton.classList.add('collapsed');
-    }
-  }  
-  
-  
-  
-  
-  
+  var mobileMenu = document.querySelector('.mobile-menu');
+  var mobileMenuButton = document.querySelector('.mobile-menu-icon button');
+
+  if (mobileMenu.style.display === 'flex') {
+    mobileMenu.style.display = 'none';
+    mobileMenuButton.innerHTML = '&#9776;'; // Ícone de três barras horizontais quando o menu está oculto
+  } else {
+    mobileMenu.style.display = 'flex';
+    mobileMenuButton.innerHTML = 'X'; // Texto "X" para representar o ícone "X"
+  }
+}
