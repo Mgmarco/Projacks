f56e5ff42b518847395b8ff2158e1d01ee18a640
mgmarco <mgmarco38@gmail.com>
2023-07-27 19:19:54 -0300

    Desenvolvimento da seção quem somos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para o navbar abrir e fechar com o botão hamburguer e X
 function menuShow() {
     var mobileMenu = document.querySelector('.mobile-menu');
     var mobileMenuButton = document.querySelector('.mobile-menu-icon button');
@@ -10,4 +11,18 @@ function menuShow() {
       mobileMenuButton.innerHTML = 'X'; // Texto "X" para representar o ícone "X"
     }
   }
+
+  // Função para os botões da seção quem somos
+  const botoesQuemSomos = document.querySelectorAll('.botao-quem-somos');
+  const conteudosQuemSomos = document.querySelectorAll('.conteudo-quem-somos');
+
+  botoesQuemSomos.forEach(botao => {
+    botao.addEventListener('click', () => {
+      const conteudoAlvo = botao.dataset.conteudo;
+      conteudosQuemSomos.forEach(conteudo => {
+        conteudo.style.display = 'none';
+      });
+      document.getElementById(conteudoAlvo).style.display = 'block';
+    });
+  });
   
