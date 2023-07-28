9ee0541d17751924a1c074ffd38df2ddc2bae482
mgmarco <mgmarco38@gmail.com>
2023-07-28 19:08:36 -0300

    Inserindo cards das propostas de trabalho

--- script.js
+++ script.js
@@ -72,4 +72,49 @@ function toggleNavbarColor() {
 
 // Adiciona um event listener para chamar a função toggleNavbarColor() quando a página é rolada
 window.addEventListener('scroll', toggleNavbarColor);
+
+
+  // Função para ajustar o posicionamento da seção "Quem Somos" após clicar no link
+  function scrollWithOffset(el) {
+    const offset = 100; // Defina o valor do offset aqui (ajuste conforme necessário)
+    const elementPosition = el.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  }
+
+  // Adicione um event listener para o clique nos links do nav-list
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Evita o comportamento padrão de clicar em um link
+
+      // Obtém o alvo do link (neste caso, a seção "Quem Somos")
+      const href = link.getAttribute('href');
+      const target = document.querySelector(href);
+
+      // Rola até o alvo com offset para evitar que a barra de navegação fixa sobreponha o conteúdo
+      if (target) {
+        scrollWithOffset(target);
+      }
+    });
+  });
+
+  // Função para controlar o efeito de slide nos serviços
+document.addEventListener("DOMContentLoaded", function() {
+  const slideElements = document.querySelectorAll(".slideN");
+
+  slideElements.forEach(function(slide, index) {
+    slide.addEventListener("click", function() {
+      slideElements.forEach(function(slide) {
+        slide.classList.remove("active-slide");
+      });
+      slide.classList.add("active-slide");
+    });
+  });
+});
+
   
